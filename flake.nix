14194ac485f827cf4c4435ba021f86cf0adb92f9
Tomasz Batko <batkot@protonmail.com>
2024-02-11 20:19:47 +0100

    Build image via flake

--- flake.nix
+++ flake.nix
@@ -10,15 +10,28 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        slim = pkg: pkg.overrideAttrs (oldAttrs: {
+          enableSharedExecutables = false;
+          postFixup = "rm -rf $out/lib $out/nix-support $out/share/doc";
+        });
         haskellPkgs = pkgs.haskellPackages.extend (self: super: {
             hangman = self.callCabal2nix "hangman" ./hangman {};
             hangman-adapters = self.callCabal2nix "hangman-adapters" ./hangman-adapters {};
-            hangman-server = self.callCabal2nix "hangman-server" ./hangman-server {};
+            hangman-server = slim (self.callCabal2nix "hangman-server" ./hangman-server {});
         });
+        server-image = pkgs.dockerTools.buildImage {
+          name = "hangman-server";
+          tag = self.rev or "dirty";
+          copyToRoot = [ hangman.server ];
+          config = {
+            Cmd = [ "hangman-server-exe" "-p 8080" ];
+          };
+        };
         hangman = {
             lib = haskellPkgs.hangman;
             adapters = haskellPkgs.hangman-adapters;
             server = haskellPkgs.hangman-server;
+            server-docker = server-image;
         };
       in
         {
