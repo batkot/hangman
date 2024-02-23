988d1dcde2d0e3b6072ee6507c998fc9d81cd6c1
Tomasz Batko <batkot@protonmail.com>
2024-02-23 21:29:48 +0100

    Pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -2,70 +2,92 @@
   description = "Overengineered Hangman example";
 
   inputs = {
-    nixpkgs.url     = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        nixpkgs-stable.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        ghc = "ghc963";
-        overlays = final: prev: {
-            elfutils = prev.elfutils.override {
-                # So that we won't end up with python3 in docker image...
-                # GHC (since 9.6.3) depends on elfutils -
-                # Elfutils with enableDebuginfod set to true (default) brings curl
-                # curl brings libpsl and libspl uses python3 as buildtool and brings it with itself
-                # What a story, Mark
-                enableDebuginfod = false;
-            };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      ghc = "ghc963";
+      overlays = final: prev: {
+        elfutils = prev.elfutils.override {
+          # So that we won't end up with python3 in docker image...
+          # GHC (since 9.6.3) depends on elfutils -
+          # Elfutils with enableDebuginfod set to true (default) brings curl
+          # curl brings libpsl and libspl uses python3 as buildtool and brings it with itself
+          # What a story, Mark
+          enableDebuginfod = false;
         };
-        pkgs = import nixpkgs { inherit system; overlays = [ overlays ]; };
-        slim-exec = pkg: pkgs.haskell.lib.justStaticExecutables pkg;
-        haskellPkgs = pkgs.haskell.packages.${ghc}.override {
-            overrides = ghcSelf: ghcSuper: {
-                generics-sop = pkgs.haskell.lib.doJailbreak ghcSuper.generics-sop;
-                hangman = ghcSuper.callCabal2nix "hangman" ./hangman {};
-                hangman-adapters = ghcSuper.callCabal2nix "hangman-adapters" ./hangman-adapters {};
-                hangman-server = slim-exec (ghcSuper.callCabal2nix "hangman-server" ./hangman-server {});
-            };
+      };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [overlays];
+      };
+      slim-exec = pkg: pkgs.haskell.lib.justStaticExecutables pkg;
+      haskellPkgs = pkgs.haskell.packages.${ghc}.override {
+        overrides = ghcSelf: ghcSuper: {
+          generics-sop = pkgs.haskell.lib.doJailbreak ghcSuper.generics-sop;
+          hangman = ghcSuper.callCabal2nix "hangman" ./hangman {};
+          hangman-adapters =
+            ghcSuper.callCabal2nix "hangman-adapters" ./hangman-adapters {};
+          hangman-server =
+            slim-exec
+            (ghcSuper.callCabal2nix "hangman-server" ./hangman-server {});
         };
-        docker-image-tag = "flake-build";
-        server-image = pkgs.dockerTools.buildImage {
-          name = "hangman-server";
-          tag = docker-image-tag;
-          copyToRoot = [ hangman.server ];
-          config = {
-            Cmd = [ "hangman-server-exe" "-p 8080" ];
-          };
+      };
+      docker-image-tag = "flake-build";
+      server-image = pkgs.dockerTools.buildImage {
+        name = "hangman-server";
+        tag = docker-image-tag;
+        copyToRoot = [hangman.server];
+        config = {Cmd = ["hangman-server-exe" "-p 8080"];};
+      };
+      hangman = {
+        lib = haskellPkgs.hangman;
+        adapters = haskellPkgs.hangman-adapters;
+        server = haskellPkgs.hangman-server;
+        server-docker = server-image;
+      };
+      pre-commit = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          stylish-haskell.enable = true;
+          alejandra.enable = true;
         };
-        hangman = {
-            lib = haskellPkgs.hangman;
-            adapters = haskellPkgs.hangman-adapters;
-            server = haskellPkgs.hangman-server;
-            server-docker = server-image;
-        };
-      in
-        {
-          packages = {
-            inherit hangman;
-            default = hangman.server;
-          };
+      };
+    in {
+      packages = {
+        inherit hangman;
+        default = hangman.server;
+      };
 
-          devShells.default = haskellPkgs.shellFor {
-            packages = p: [hangman.lib hangman.adapters hangman.server];
+      devShells.default = haskellPkgs.shellFor {
+        packages = p: [hangman.lib hangman.adapters hangman.server];
 
-            buildInputs = with pkgs.haskellPackages; [
-              haskell-language-server
-              cabal-install
-              pkgs.just
-            ];
+        buildInputs = with pkgs.haskellPackages; [
+          haskell-language-server
+          cabal-install
+          pkgs.just
+        ];
 
-            withHoogle = false;
-            shellHook = ''
-                export FLAKE_IMAGE_TAG=${docker-image-tag}
-            '';
-          };
-        }
-  );
+        withHoogle = false;
+        shellHook = ''
+          ${pre-commit.shellHook}
+          export FLAKE_IMAGE_TAG=${docker-image-tag}
+        '';
+      };
+    });
 }
--- justfile
+++ justfile
@@ -1,4 +1,4 @@
-set dotenv-load
+set dotenv-load := true
 
 revision := ```
     commit_sha=$(git rev-parse HEAD)
@@ -8,7 +8,6 @@ revision := ```
         echo "${commit_sha}-dirty"
     fi
     ```
-
 docker-image := 'server-image'
 
 default:
@@ -21,13 +20,13 @@ build: hpack
     cabal build all
 
 build-image:
-    nix build ".#hangman.server-docker" -o {{docker-image}}
+    nix build ".#hangman.server-docker" -o {{ docker-image }}
 
 release: build-image
-    @echo "Deploying {{revision}} to: {{env('DOCKER_HOST', 'local')}}"
-    docker load < {{docker-image}}
-    docker tag hangman-server:{{env('FLAKE_IMAGE_TAG')}} hangman-server:{{revision}}
-    docker tag hangman-server:{{revision}} hangman-server:latest
-    docker image rm hangman-server:{{env('FLAKE_IMAGE_TAG')}}
-    docker-compose -f {{env('DOCKER_COMPOSE_FILE', 'docker-compose.yaml -f docker-compose.local.yaml')}} up -d --remove-orphans
-    rm {{docker-image}}
+    @echo "Deploying {{ revision }} to: {{ env('DOCKER_HOST', 'local') }}"
+    docker load < {{ docker-image }}
+    docker tag hangman-server:{{ env('FLAKE_IMAGE_TAG') }} hangman-server:{{ revision }}
+    docker tag hangman-server:{{ revision }} hangman-server:latest
+    docker image rm hangman-server:{{ env('FLAKE_IMAGE_TAG') }}
+    docker-compose -f {{ env('DOCKER_COMPOSE_FILE', 'docker-compose.yaml -f docker-compose.local.yaml') }} up -d --remove-orphans
+    rm {{ docker-image }}
